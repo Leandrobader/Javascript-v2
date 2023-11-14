9edd3f7f8400d725402770053d848266a5eb2896
Leanbader <baderleandro@gmail.com>
2023-11-14 00:53:02 -0300

    comenzamos con los primeros conceptos de js

--- js/appjs.js
+++ js/appjs.js
@@ -1,1 +1,46 @@
 //Aqui vamos a comenzar con Javascript
+
+console.log("Hola mundo");
+
+//Tipos de datos
+
+//string: cadenas de caracteres (se lo representa porque esta contenido entre comillas simples o dobles)
+//number: todos los numeros
+//Booleanos: son logicos y solo toman dos valores (true o false)
+//Array:
+//Object
+//Empty: null, nada
+
+//Variables (hay tres tipos: var, let, const)
+//var: deprecado, ya no se usa
+//crear o declarar una variable
+
+//sintaxis:
+/*
+tipo de variable (nombre de la variable en CamelCase o nombre-de-la-var o nombre_de_la_var) = string, numbre, boolean, array o Object
+
+*/
+
+let mensaje = "Esto es un mensaje"; //le puedo cambiar el valor
+console.log("let mensaje:", mensaje);
+document.write("<br>" + mensaje)
+const token = "fdsdffgdf"; //no le puedo cambiar el valor
+
+//cambiar valor de una variable
+mensaje = "Esto es otro mensaje distinto"
+console.log(mensaje);
+document.write("<br>" + mensaje)
+
+mensaje = 16;
+console.log(mensaje);
+document.write("<br>" + mensaje)
+
+//token = 1234 No se puede cabiar el valor de una constante
+
+//imprimir datos en el documento HTML
+
+//document.write("Esto es un document.write")
+
+
+
+
